c0afa38f8168f25127656c7e484ba8d24b9d880c
seidaliev <seidaliev.work@gmail.com>
2022-03-24 14:04:09 +0600

    Created directories - templates (index.html) - fonts - styles - images and add some dependencies in pom.xml

---
+++ src/test/java/com/seidaliev/foodsharing/FoodsharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seidaliev.foodsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
